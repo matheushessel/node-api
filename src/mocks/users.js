a1db94d6b89243908e103297606e1e173ee75548
Matheus Hessel <matheushqueiroz@gmail.com>
2023-11-11 16:51:41 -0300

    Deploy do projeto

---
+++ src/mocks/users.js
@@ -0,0 +1,16 @@
+//Base de dados para exemplos
+
+module.exports = [
+    {
+        id: 1,
+        name: 'Matheus',
+    },
+    {
+        id: 2,
+        name: 'Carlos',
+    },
+    {
+        id: 3,
+        name: 'Bruno',
+    },
+];
